a81db597043e2236eb8c08d94b5b02eeb3900aa1
Wyverl <t.p.rainteau@gmail.com>
2024-12-05 13:00:07 +0100

    creation of the crud for the travel

---
+++ api/src/travel/travel.controller.ts
@@ -0,0 +1,37 @@
+import Router from "express";
+import { findAll, findOne, create , update } from "./travel.service";
+const router = Router();
+
+// Get all travel
+router.get("/", async (req, res) => {
+  const travels = await findAll();
+
+  res.send(travels);
+});
+
+// Get one travel
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const travel = await findOne(+id);
+
+  res.send(travel);
+});
+
+// Create a travel
+router.post("/", async (req, res) => {
+  const travelInfo = req.body;
+    
+  const travel = await create(travelInfo);
+  res.send(travel);
+});
+
+// Update a travel
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const travelInfo = req.body;
+    
+  const travel = await update(+id,travelInfo);
+  res.send(travel);
+});
+
+export default router;
